bfa1aeda2fbf954867c55fc59328c934f89284db
kanellov <contact@kanellov.com>
2016-02-14 02:09:08 +0200

    finalized; needs unit testing

--- src/AddMiddleware.php
+++ src/AddMiddleware.php
@@ -1,16 +1,14 @@
 <?php
 /**
- * kanellov/slim-skeleton
+ * kanellov/slim-modules
  * 
- * @link https://github.com/kanellov/slim-skeleton for the canonical source repository
+ * @link https://github.com/kanellov/slim-modules for the canonical source repository
  * @copyright Copyright (c) 2016 Vassilis Kanellopoulos (http://kanellov.com)
  * @license GNU GPLv3 http://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
 
-namespace Knlv\Slim;
+namespace Knlv\Slim\Modules;
 
-use Knlv\Utils\PriorityComparatorTrait;
-use Knlv\Utils\ValidateCallableTrait;
 use Slim\App;
 
 class AddMiddleware
--- src/MergeArrays.php
+++ src/MergeArrays.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * kanellov/slim-skeleton
+ * kanellov/slim-modules
  * 
- * @link https://github.com/kanellov/slim-skeleton for the canonical source repository
+ * @link https://github.com/kanellov/slim-modules for the canonical source repository
  * @copyright Copyright (c) 2016 Vassilis Kanellopoulos (http://kanellov.com)
  * @license GNU GPLv3 http://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
 
-namespace Knlv\Utils;
+namespace Knlv\Slim\Modules;
 
 /**
  * Merges the values of the $new array into $merged array
